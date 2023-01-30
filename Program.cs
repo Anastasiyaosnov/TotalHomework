8b364614b4832472cdf63be43d0c2ab98918a43b
Anastasiya <anastasiyaosnov.mail.ru>
2023-01-30 20:56:06 +0300

    new method create array where every element from user array less or equal 3. Need to limit the number of alement in new array

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     return Console.ReadLine();
 }
 
-string[] CreateArray(int lenght)
+string[] FillArray(int lenght)
 {
     string[] first = new string[lenght];
     for (int i = 0; i < first.Length; i++)
@@ -20,12 +20,42 @@ void PrintArray(string[] array)
         Console.Write($"'{array[i]}' ");
 }
 
+string [] CreateArray (string[] array)
+{
+    string[] TotalArray = new string[array.Length];
+    int counter = 0;
+    int signs = 0;
+    int TotalCounter = 0;
+    while (counter < TotalArray.Length)
+    {
+        foreach(int el in array[counter])
+        {
+            signs ++; 
+        }
+        if(signs<= 3)
+        {
+            TotalArray[TotalCounter] = array[counter];
+            TotalCounter ++;
+        }
+        signs = 0;
+        counter++;
+    }
+    return TotalArray;
+}
+
+
+
 Console.WriteLine("Планируется сформировать массив из некоторого количества элементов (слов, цифр, предложений и т.д.). На данном этапе необходимо определить, сколько в этом массиве будет элементов. Введите количество элементов в массиве");
 int UserLenght =  Convert.ToInt32(Console.ReadLine());
 
-string[] UserArray = CreateArray(UserLenght);
+string[] UserArray = FillArray(UserLenght);
 
 Console.WriteLine("Получился следующий массив строк");
 
 PrintArray(UserArray);
 
+string[] ResultArray = CreateArray(UserArray);
+
+Console.WriteLine();
+
+PrintArray(ResultArray);
