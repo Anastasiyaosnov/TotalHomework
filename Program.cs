dd4b6f1ee2f0297038b71d7d1a057a0e34c2a2c2
Anastasiya <anastasiyaosnov.mail.ru>
2023-01-30 20:00:50 +0300

    create method to fill array by user

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿string Promt(string massage)
+{
+    Console.WriteLine($"{massage}");
+    return Console.ReadLine();
+}
+
+string[] CreateArray(int lenght)
+{
+    string[] first = new string[lenght];
+    for (int i = 0; i < first.Length; i++)
+    {
+        first[i] = Promt($"Введите строку {i}");
+    }
+    return first;
+}
+
+Console.WriteLine("Планируется сформировать массив из некоторого количества элементов (слов, цифр, предложений и т.д.). На данном этапе необходимо определить, сколько в этом массиве будет элементов. Введите количество элементов в массиве");
+int UserLenght =  Console.ReadLine();
