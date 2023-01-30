5de62f72f4a02cd108397fd680dab42bc7aa1262
Anastasiya <anastasiyaosnov.mail.ru>
2023-01-30 20:09:07 +0300

    design possibility to fill array by user

--- Program.cs
+++ Program.cs
@@ -9,10 +9,23 @@ string[] CreateArray(int lenght)
     string[] first = new string[lenght];
     for (int i = 0; i < first.Length; i++)
     {
-        first[i] = Promt($"Введите строку {i}");
+        first[i] = Promt($"Введите строку {i+1}");
     }
     return first;
 }
 
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"'{array[i]}' ");
+}
+
 Console.WriteLine("Планируется сформировать массив из некоторого количества элементов (слов, цифр, предложений и т.д.). На данном этапе необходимо определить, сколько в этом массиве будет элементов. Введите количество элементов в массиве");
-int UserLenght =  Console.ReadLine();
+int UserLenght =  Convert.ToInt32(Console.ReadLine());
+
+string[] UserArray = CreateArray(UserLenght);
+
+Console.WriteLine("Получился следующий массив строк");
+
+PrintArray(UserArray);
+
